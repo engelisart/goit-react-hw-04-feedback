f7b2e03b4d9487c6719556abbbc178b5b1db1147
engelisart <144841027+engelisart@users.noreply.github.com>
2023-12-13 12:43:48 +0100

    fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,13 +12,13 @@ export const App = () => {
   const handleFeedback = type => {
     switch (type) {
       case 'good':
-        setGood(good + 1);
+        setGood(prevGood => prevGood + 1);
         break;
       case 'neutral':
-        setNeutral(neutral + 1);
+        setNeutral(prevNeutral => prevNeutral + 1);
         break;
       case 'bad':
-        setBad(bad + 1);
+        setBad(prevBad => prevBad + 1);
         break;
       default:
     }
